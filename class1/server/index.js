4fb4a40be512ea79dba61de2de3c618e52cb2c22
jael-dev <jaelngaleu@gmail.com>
2023-04-01 00:04:49 +0100

    expense schema created

--- class1/server/index.js
+++ class1/server/index.js
@@ -4,3 +4,5 @@ const mongoose = require('mongoose')
 const app = express()
 
 const mongoDbAccess = "mongodb+srv://admin:admin@cluster0.0g8nv2k.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(mongoDbAccess, {useNewUrlParser : true}).then(()=> console.log("connected to the database")).catch((e)=> console.log(e))
