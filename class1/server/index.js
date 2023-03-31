66924f670e55b3da58e92e578c7739161d9cac20
jael-dev <jaelngaleu@gmail.com>
2023-03-31 23:28:53 +0100

    mongoose installation

---
+++ class1/server/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const mongoDbAccess = "mongodb+srv://admin:admin@cluster0.0g8nv2k.mongodb.net/?retryWrites=true&w=majority"
